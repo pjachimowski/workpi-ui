7d85e3165fe629a17f29996e758bd145e14faa07
pjachimowski <p.jachimowski89@gmail.com>
2020-10-28 18:24:24 +0100

    interface User added. Data hardcoded. Started wirking with hooks. I also went through backend to understand how API is connected with front using GraphQL

--- src/components/Profile/PersonalInfo/PersonalInfo.tsx
+++ src/components/Profile/PersonalInfo/PersonalInfo.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
-import './PersonalInfo.scss';
-import profile from './profile.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from "react";
+import "./PersonalInfo.scss";
+import profile from "./profile.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useUserCtx } from "../../../hooks/UserHook";
+
+interface User {
+  name: string;
+  currentJob: string;
+  futureRelevance: string;
+  department: string;
+  departmentLocation: string;
+}
 
 const PersonalInfo = () => {
+  const [user, setUser]: [User, (user: User) => void] = useState({
+    name: "",
+    currentJob: "",
+    futureRelevance: "",
+    department: "",
+    departmentLocation: "",
+  });
+
+  useEffect(() => {
+    setUser({
+      name: "Patryk Jachimowski",
+      currentJob: "Creative Director",
+      futureRelevance: "very probably disappearing",
+      department: "Marketing",
+      departmentLocation: "Amsterdam",
+    });
+  });
+
   return (
     <div className="personal-info">
       <img src={profile} alt="profile picture"></img>
       <div className="name">Patryk Jachimowski</div>
-      <div className="current-job"><FontAwesomeIcon className="icon" icon={['fas', 'briefcase']}></FontAwesomeIcon>Creative Director</div>
-      <div className="future-relevance"><FontAwesomeIcon className="icon" icon={['fas', 'forward']}></FontAwesomeIcon> very probably disappearing</div>
-      <div className="department"><FontAwesomeIcon className="icon" icon={['fas', 'building']}></FontAwesomeIcon> Marketing</div>
-      <div className="department-location"><FontAwesomeIcon className="icon" icon={['fas', 'map-marker-alt']}></FontAwesomeIcon> Amsterdam</div>
-      <a href="#"><FontAwesomeIcon className="icon" icon={['fas', 'plus-square']}></FontAwesomeIcon>Add field</a>
+      <div className="current-job">
+        <FontAwesomeIcon
+          className="icon"
+          icon={["fas", "briefcase"]}
+        ></FontAwesomeIcon>
+        Creative Director
+      </div>
+      <div className="future-relevance">
+        <FontAwesomeIcon
+          className="icon"
+          icon={["fas", "forward"]}
+        ></FontAwesomeIcon>{" "}
+        very probably disappearing
+      </div>
+      <div className="department">
+        <FontAwesomeIcon
+          className="icon"
+          icon={["fas", "building"]}
+        ></FontAwesomeIcon>{" "}
+        Marketing
+      </div>
+      <div className="department-location">
+        <FontAwesomeIcon
+          className="icon"
+          icon={["fas", "map-marker-alt"]}
+        ></FontAwesomeIcon>{" "}
+        Amsterdam
+      </div>
+      <a href="#">
+        <FontAwesomeIcon
+          className="icon"
+          icon={["fas", "plus-square"]}
+        ></FontAwesomeIcon>
+        Add field
+      </a>
     </div>
   );
 };
