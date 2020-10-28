552ed195ee0507d269fedb9ab4cee51c61539899
pjachimowski <p.jachimowski89@gmail.com>
2020-10-28 16:26:26 +0100

    mockup text, img and icons in Personal Info Component. First Styling. Understanding styling concepts of the project

--- src/components/Profile/PersonalInfo/PersonalInfo.tsx
+++ src/components/Profile/PersonalInfo/PersonalInfo.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import './PersonalInfo.scss';
+import profile from './profile.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const PersonalInfo = () => {
   return (
-    <div>
-      <h1>Personal Info</h1>
-      <div>content</div>
+    <div className="personal-info">
+      <img src={profile} alt="profile picture"></img>
+      <div className="name">Patryk Jachimowski</div>
+      <div className="current-job"><FontAwesomeIcon className="icon" icon={['fas', 'briefcase']}></FontAwesomeIcon>Creative Director</div>
+      <div className="future-relevance"><FontAwesomeIcon className="icon" icon={['fas', 'forward']}></FontAwesomeIcon> very probably disappearing</div>
+      <div className="department"><FontAwesomeIcon className="icon" icon={['fas', 'building']}></FontAwesomeIcon> Marketing</div>
+      <div className="department-location"><FontAwesomeIcon className="icon" icon={['fas', 'map-marker-alt']}></FontAwesomeIcon> Amsterdam</div>
+      <a href="#"><FontAwesomeIcon className="icon" icon={['fas', 'plus-square']}></FontAwesomeIcon>Add field</a>
     </div>
   );
 };
